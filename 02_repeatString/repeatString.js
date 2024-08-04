a859418c3c2eeaea17acf8810715ace57aac2e84
Cody Blanchard <codydblanchard@gmail.com>
2024-08-04 11:51:04 -0500

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  i = 0;
+  finalString = "";
+  if (num < 0) {
+    finalString = "ERROR";
+  }
+  while (i < num) {
+    finalString += str;
+    i++;
+  }
+  return finalString;
 };
 
 // Do not edit below this line

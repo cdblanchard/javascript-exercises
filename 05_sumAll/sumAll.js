7a76a446cfde0a6743016d1b4d44b8a072b7d1a3
Cody Blanchard <codydblanchard@gmail.com>
2024-08-05 16:53:11 -0500

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let bigNum;
+  let smallNum;
+  if (num1 < num2) {
+    smallNum = num1;
+    bigNum = num2;
+  } else {
+    smallNum = num2;
+    bigNum = num1;
+  }
+  if (
+    !Number.isInteger(smallNum) ||
+    !Number.isInteger(bigNum) ||
+    smallNum < 0 ||
+    bigNum < 0
+  ) {
+    return "ERROR";
+  } else {
+    let sum = 0;
+    i = smallNum;
+    while (i <= bigNum) {
+      sum += i;
+      i++;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line

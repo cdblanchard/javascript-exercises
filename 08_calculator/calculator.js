cd2afada08c643929e39040bf4c7e7f620a94f38
Cody Blanchard <codydblanchard@gmail.com>
2024-08-09 15:32:59 -0500

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (x) {
+  if (x.length === 0) {
+    return 0;
+  } else {
+    return x.reduce((a, b) => a + b, 0);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (x) {
+  return x.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  let finalNum = x;
+  for (i = 1; i < y; i++) {
+    finalNum *= x;
+  }
+  return finalNum;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  let result = x;
+
+  if (x === 0 || x === 1) {
+    return 1;
+  }
+  while (x > 1) {
+    x--;
+    result = result * x;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
